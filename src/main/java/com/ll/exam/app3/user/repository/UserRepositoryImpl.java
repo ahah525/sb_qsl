7fcb0be8d1019f8208df4802b63eef5d0c3a1161
ahah525 <ahah525@naver.com>
2022-08-28 21:57:10 +0900

    feat : JPAQueryFactory Bean 등록

--- src/main/java/com/ll/exam/app3/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/app3/user/repository/UserRepositoryImpl.java
@@ -1,4 +1,27 @@
 package com.ll.exam.app3.user.repository;
 
+import com.ll.exam.app3.user.domain.QSiteUser;
+import com.ll.exam.app3.user.domain.SiteUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+// 이름은 무조건 ~Impl 이어야함
+@RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+    @Override
+    public SiteUser getQslUser(Long id) {
+        /**
+         * SELECT *
+         * FROM site_user
+         * WHERE id = 1
+         */
+//        return jpaQueryFactory
+//                .select(QSiteUser.siteUser)
+//                .from(QSiteUser.siteUser)
+//                .where(QSiteUser.siteUser.id.eq(1L))
+//                .fetch();
+        return null;
+
+    }
 }
