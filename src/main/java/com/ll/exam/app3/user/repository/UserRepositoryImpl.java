d791e930cef04d5ad661e0dde63ef22149517eb5
ahah525 <ahah525@naver.com>
2022-08-28 22:11:25 +0900

    refactor : static import로 DSL 간소화

--- src/main/java/com/ll/exam/app3/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/ll/exam/app3/user/repository/UserRepositoryImpl.java
@@ -1,9 +1,10 @@
 package com.ll.exam.app3.user.repository;
 
-import com.ll.exam.app3.user.domain.QSiteUser;
 import com.ll.exam.app3.user.domain.SiteUser;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import static com.ll.exam.app3.user.domain.QSiteUser.*;
 
 // 이름은 무조건 ~Impl 이어야함
 @RequiredArgsConstructor
@@ -17,9 +18,9 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
          * WHERE id = 1
          */
         return jpaQueryFactory
-                .select(QSiteUser.siteUser)
-                .from(QSiteUser.siteUser)
-                .where(QSiteUser.siteUser.id.eq(id))
+                .select(siteUser)
+                .from(siteUser)
+                .where(siteUser.id.eq(id))
                 .fetchOne();    // 단건 조회(없으면 null 반환)
     }
 }
