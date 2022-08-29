4bddee6172c8c41edb530015e15e3fe158b9004a
ahah525 <ahah525@naver.com>
2022-08-29 13:32:51 +0900

    feat : 테스트 모드 용 초기 데이터 세팅을 위해 TestInitData 도입

---
+++ src/main/java/com/ll/exam/app3/base/TestInitData.java
@@ -0,0 +1,34 @@
+package com.ll.exam.app3.base;
+
+import com.ll.exam.app3.user.domain.SiteUser;
+import com.ll.exam.app3.user.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")    // 해당 클래스의 빈들은 테스트 모드에서만 활성화
+public class TestInitData {
+    // CommandLineRunner: 주로 앱 실행 직후 초기데이터 세팅, 초기화에 사용
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            SiteUser u1 = SiteUser.builder()
+                    .username("user1")
+                    .password("{noop}1234")
+                    .email("user1@test.com")
+                    .build();
+
+            SiteUser u2 = SiteUser.builder()
+                    .username("user2")
+                    .password("{noop}1234")
+                    .email("user2@test.com")
+                    .build();
+
+            userRepository.saveAll(Arrays.asList(u1, u2));
+        };
+    }
+}
--- src/test/java/com/ll/exam/app3/user/repository/UserRepositoryTest.java
+++ src/test/java/com/ll/exam/app3/user/repository/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 
@@ -12,26 +13,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test") // 테스트 모드 활성화
 class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    void save() {
+    @DisplayName("회원 생성")
+    void t1() {
         // {noop}: 암호화되지 않은 평문 비밀번호
-        SiteUser u1 = SiteUser.builder()
-                .username("user1")
+        SiteUser u3 = SiteUser.builder()
+                .username("user3")
                 .password("{noop}1234")
-                .email("user1@test.com")
+                .email("user3@test.com")
                 .build();
 
-        SiteUser u2 = SiteUser.builder()
-                .username("user2")
+        SiteUser u4 = SiteUser.builder()
+                .username("user4")
                 .password("{noop}1234")
-                .email("user2@test.com")
+                .email("user4@test.com")
                 .build();
 
-        userRepository.saveAll(Arrays.asList(u1, u2));
+        userRepository.saveAll(Arrays.asList(u3, u4));
     }
 
     @Test
